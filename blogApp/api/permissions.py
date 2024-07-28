f63ec5e2f46c7ec6e29e034519df8f179a6665c2
selcuk <yilmax4@gmail.com>
2024-07-28 17:23:40 +0300

    base permissions and custom permissions

---
+++ blogApp/api/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+
+class IsPostOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user == obj.author
+
+
+class IsAdminUserOrReadOnly(permissions.IsAdminUser):
+    def has_permission(self, request, view):
+        is_admin = super().has_permission(request, view)
+        return request.method in permissions.SAFE_METHODS or is_admin
--- blogApp/api/views.py
+++ blogApp/api/views.py
@@ -19,8 +19,8 @@ from .serializers import (
     # ViewSerializer
 )
 from rest_framework import permissions
+from .permissions import IsPostOwnerOrReadOnly, IsAdminUserOrReadOnly
 # from .pagination import CustomLimitOffsetPagination
-# from .permissions import IsPostOwnerOrReadOnly, IsAdminUserOrReadOnly
 from django.contrib.auth import get_user_model
 # User = settings.AUTH_USER_MODEL
 User = get_user_model()
@@ -29,13 +29,13 @@ User = get_user_model()
 class CategoryView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    # permission_classes = [IsAdminUserOrReadOnly]
+    permission_classes = [IsAdminUserOrReadOnly]
 
 class BlogPostView(generics.ListCreateAPIView):
     queryset = BlogPost.objects.filter(status="p")
     serializer_class = BlogPostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     # pagination_class = CustomLimitOffsetPagination
-    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user) 
@@ -44,7 +44,7 @@ class BlogPostDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
     lookup_field = "slug"
-    # permission_classes = [IsPostOwnerOrReadOnly]
+    permission_classes = [IsPostOwnerOrReadOnly]
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -56,7 +56,7 @@ class BlogPostDetailView(generics.RetrieveUpdateDestroyAPIView):
 class CommentView(generics.CreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         slug = self.kwargs.get('slug')
@@ -90,7 +90,7 @@ class LikeView(generics.ListCreateAPIView):
 class UserAllPosts(generics.ListAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
-    # permission_classes = [IsPostOwnerOrReadOnly]
+    permission_classes = [IsPostOwnerOrReadOnly]
 
     def get_queryset(self):
         author = self.request.user
@@ -100,4 +100,4 @@ class UserAllPosts(generics.ListAPIView):
 class PostUserView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = PostUserSerializer
-    # permission_classes = [permissions.IsAuthenticated]     
+    permission_classes = [permissions.IsAuthenticated]     
