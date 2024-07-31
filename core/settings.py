98a5d5d5c6b869d1d78918807877bde473c9b931
selcuk <yilmax4@gmail.com>
2024-07-31 15:41:23 +0300

    corsheader packaget and loging folder added

--- core/settings.py
+++ core/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
+
 
 
 # Application definition
@@ -44,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'dj_rest_auth',
     'django_filters',
+    'corsheaders',
     #myApps
     'blogApp',
     'users',    
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -152,3 +153,76 @@ REST_AUTH = {
 #         'rest_framework.filters.OrderingFilter', # Global settings for Ordering
 #     ]
 # }
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG')
+if DEBUG:
+    THIRD_PARTY_APPS = ["debug_toolbar"]
+    INSTALLED_APPS += THIRD_PARTY_APPS
+
+    THIRD_PARTY_MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+    MIDDLEWARE += THIRD_PARTY_MIDDLEWARE
+
+    INTERNAL_IPS = [ 
+    "127.0.0.1", 
+    ]
+    LOGGING = {
+    "version": 1,
+    # is set to True then all loggers from the default configuration will be disabled.
+    "disable_existing_loggers": True,
+    # Formatters describe the exact format of that text of a log record. 
+    "formatters": {
+        "standard": {
+            "format": "[%(levelname)s] %(asctime)s %(name)s: %(message)s"
+        },
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    # The handler is the engine that determines what happens to each message in a logger.
+    # It describes a particular logging behavior, such as writing a message to the screen, 
+    # to a file, or to a network socket.
+    "handlers": {
+        "console": {                              #CONSOLE DA GÖRMEK İSTEDİĞİN LOG KAYITLARI ÖZELLİKLERİ
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+            "level": "ERROR",
+            "stream": "ext://sys.stdout",
+            },
+        'file': {                               #FİLE HALİNDE GÖRMEK İSTEDİĞİN LOG KAYITLARI ÖZELLİKLERİ ana dizinde debug.log içine yazar         
+            'class': 'logging.FileHandler',
+            "formatter": "verbose",
+            'filename': './debug.log',
+            'level': 'INFO',                       # BURAYI İSTEĞİNE GÖRE AYARLAYABİLİRSİN. INFO WARNING ERROR CRITICAL ...VS
+        }, 
+    },
+    # A logger is the entry point into the logging system.
+    "loggers": {
+        "django": {
+            "handlers": ["console", 'file'],
+            # log level describes the severity of the messages that the logger will handle. 
+            "level": config("DJANGO_LOG_LEVEL", "INFO"),
+            'propagate': True,
+            # If False, this means that log messages written to django.request 
+            # will not be handled by the django logger.
+        },
+    },
+    }
+
+
+#backend'i dışarıya açmak için settings e elenecek leri
+ALLOWED_HOSTS = ['*']
+CORS_ALLOW_ALL_ORIGINS=True  
+#hangi metodlara izin vermek istiyorsun belirtiyoruz.
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]    
